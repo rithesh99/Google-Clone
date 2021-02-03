4aeede633a831b4281107323f76529e0da3b6985
rithesh99 <ritheshdavid@gmail.com>
2021-02-03 17:56:30 +0530

    Deployed @firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from "react"
-import './App.css';
-import Home from "./pages/Home"
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SearchPage from "./pages/Search/SearchPage";
+
 function App() {
   return (
     <div className="App">
-      <Home/>
-      {/* Home */}
-
-      {/* SearchPage */}
-
-       
-
+      <BrowserRouter>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/keys/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyAfr76dq7CPP7mWZ5Tre51UlaQ4jZLBiGs";
+export const CONTEXT_KEY = "8ed4df6dbbefa581c";
+
